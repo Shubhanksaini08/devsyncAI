5d661ee8c0244bf294f7361a8d8baab808342f09
astikgupta <guptastik123@gmail.com>
2026-04-08 16:44:46 +0530

    Update redis.service.js

--- backend/services/redis.service.js
+++ backend/services/redis.service.js
@@ -1,18 +1,20 @@
 import Redis from "ioredis";
 
-const redisClient = new Redis({
+const redis = new Redis({
   host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD
+  port: parseInt(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD,
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
-redisClient.on("connect", () => {
-  console.log("Redis connected");
+redis.on("connect", () => {
+  console.log("Redis connected ✅");
 });
 
-redisClient.on("error", (err) => {
+redis.on("error", (err) => {
   console.error("Redis error:", err);
 });
 
-export default redisClient;
+export default redis;
